76cc60dd403093ef436d6218ce7a2fe5fd84a8e5
Abhishek Dutt <duttabhishek0@gmail.com>
2023-04-02 02:55:02 +0530

    Updated Setup

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="pdf-to-excel",
     version="1.0.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Abhishek Dutt",
+    author_email="duttabhishek0@gmail.com",
     description="A desktop application to convert PDF files to Excel files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/pdf-to-excel",
+    url="https://github.com/duttabhishek0/pdf2excel/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     entry_points={
         "gui_scripts": [
-            "pdf-to-excel=pdf_to_excel.gui:main"
+            "pdf-to-excel=pdf_to_excel.ui:main_window"
         ]
     },
     package_data={
